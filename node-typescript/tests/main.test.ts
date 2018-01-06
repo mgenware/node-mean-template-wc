a4341bee4ff4424c06e04911ad189022628d6f09
Mgen <m@mgenware.com>
2018-01-06 20:17:50 +0800

    Add another test file

---
+++ node-typescript/tests/main.test.ts
@@ -0,0 +1,5 @@
+import * as main from '../lib/main';
+
+it('test add', () => {
+  expect(main.add(1, -9)).toBe(-8);
+});
--- node-typescript/tests/module.test.ts
+++ node-typescript/tests/module.test.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs';
 
 describe('require this module', () => {
   it('Verify module members', () => {
-    // TODO: replace main.default to one of your module members
-    expect(typeof main.default).toBe('function');
+    // TODO: replace "main.add" to one of your module members
+    expect(typeof main.add).toBe('function');
   });
 
   it('Verify type definition files', () => {
