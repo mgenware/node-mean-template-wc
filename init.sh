557559f44f8a4bb2e3ee3436178c3f2a2d064b5d
Mgen <m@mgenware.com>
2018-04-02 16:04:59 +0800

    Use rimraf to delete the dist directory

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-yarn add typescript jest tslint copyfiles @types/node @types/jest -D --production && rm -- "$0"
+yarn add typescript jest tslint copyfiles rimraf @types/node @types/jest -D --production && rm -- "$0"
