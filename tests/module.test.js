9b474f07c194c547ce035b15e6437d30837370ee
Mgen <m@mgenware.com>
2018-05-16 18:24:20 +0800

    Update type definition file location

--- tests/module.test.js
+++ tests/module.test.js
@@ -8,6 +8,6 @@ describe('require this module', () => {
   });
 
   test('Verify type definition files', () => {
-    expect(fs.statSync('./dist/lib/main.d.ts').isFile()).toBeTruthy();
+    expect(fs.statSync('./dist/main.d.ts').isFile()).toBeTruthy();
   });
 });
