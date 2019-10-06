6fd43b537607c26dbaa984a5b819d2d288b06328
Mgen <m@mgenware.com>
2019-10-06 21:54:43 +0800

    Migrate from TSLint from ESLint

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-yarn add typescript mocha tslint del-cli cross-env @types/node @types/mocha -D --production && rm -- "$0"
+yarn add typescript mocha del-cli cross-env @types/node @types/mocha eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin -D && rm -- "$0"
