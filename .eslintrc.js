f7fd03eb50abacc5b38aa2bd3ed90a2b2726aa42
Mgen <m@mgenware.com>
2020-04-16 01:24:51 +0800

    Enable @typescript-eslint/switch-exhaustiveness-check rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-console': ['error', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
   },
 };
