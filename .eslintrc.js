3372c04fb50a9356008f90883a9ac52d6061677d
Mgen <m@mgenware.com>
2020-05-24 23:36:18 +0800

    Use more restrictive typescript eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,25 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
+  extends: ['airbnb-typescript/base', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
     project: './tsconfig.json',
   },
   rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-plusplus': 'off',
+    // Allow `for-of` loops.
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    // We'll let prettier handle operator line breaks.
+    'operator-linebreak': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-console': ['error', { allow: ['warn', 'error'] }],
     '@typescript-eslint/switch-exhaustiveness-check': 'error',
   },
 };
