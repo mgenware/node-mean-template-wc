3a4d2ff3deae2e4008e393f7946ea9d963c82e61
Mgen <m@mgenware.com>
2020-05-30 19:16:16 +0800

    Migrate to daizong

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-yarn add typescript mocha rimraf cross-env "@types/node" "@types/mocha" source-map-support eslint "@typescript-eslint/parser" "@typescript-eslint/eslint-plugin" "eslint-config-airbnb-typescript" "eslint-plugin-import" -D && rm -- "$0"
+yarn add typescript mocha rimraf cross-env "@types/node" "@types/mocha" source-map-support eslint "@typescript-eslint/parser" "@typescript-eslint/eslint-plugin" "eslint-config-airbnb-typescript" "eslint-plugin-import" daizong -D && rm -- "$0"
