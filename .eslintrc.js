68a7db7359ffbbf101c11007e2e16246298233db
Mgen <m@mgenware.com>
2021-01-03 19:46:17 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['airbnb-typescript-lite', 'plugin:@typescript-eslint/recommended', 'mgenware'],
   parserOptions: {
-    ecmaVersion: 2018,
     sourceType: 'module',
     project: './tsconfig.json',
   },
